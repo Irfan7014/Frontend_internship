cd64021a22c7be27d8cc8504cf8932a350b48dda
Syed Irfan Amjad Abidi <irfanabidi7@gmail.com>
2022-01-29 21:44:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/frontend_internship/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.frontend_internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
